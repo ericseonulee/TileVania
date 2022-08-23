ed083ab37ba868d846e45b46252e9af84c9a15d5
Eric Lee <szl0094@auburn.edu>
2022-08-23 16:15:52 -0500

    player ability to climb ladder added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
+    [SerializeField] private float climbSpeed = 5f;
     [SerializeField] private float runSpeed = 5f;
     [SerializeField] private float jumpSpeed = 5f;
+    private bool isPlayerClimbing;
+    private bool isPlayerOnLadder;
+    private bool isPlayerOnGround;
+    private bool canJump;
+    private float gravityScaleAtStart = 1f;
 
     Vector2 moveInput;
     Rigidbody2D playerRigidbody;
@@ -21,6 +27,8 @@ public class PlayerMovement : MonoBehaviour {
         if (playerRigidbody == null) {
             Debug.LogError("Player Rigidbody2D is null.");
         }
+        gravityScaleAtStart = playerRigidbody.gravityScale;
+
 
         if (playerAnimator == null) {
             Debug.LogError("Player Animator is null."); 
@@ -32,7 +40,46 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void Update() {
+        isPlayerOnLadder = playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"));
+        isPlayerOnGround = playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        canJump = isPlayerOnGround || isPlayerClimbing;
+
         Run();
+
+        if (isPlayerOnLadder && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))) {
+            isPlayerClimbing = true;
+        }
+        ClimbLadder();
+    }
+
+    private void ClimbLadder() {
+        if (!isPlayerOnLadder) {
+            isPlayerClimbing = false;
+            playerAnimator.SetBool("isClimbing", false);
+            playerRigidbody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        if (isPlayerClimbing) {
+            Vector2 playerVelocity = new Vector2(playerRigidbody.velocity.x, moveInput.y * climbSpeed);
+            playerRigidbody.velocity = playerVelocity;
+
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)) {
+                playerRigidbody.gravityScale = 0;
+                playerAnimator.SetBool("isClimbing", true);
+            }
+            
+            if (isPlayerMovingVertical()) {
+                if (isPlayerOnGround) {
+                    playerAnimator.SetBool("isClimbing", false);
+                }// also stop animator when top of ladder.
+
+                playerAnimator.StopPlayback();
+            }
+            else {
+                playerAnimator.StartPlayback();
+            }
+        }
     }
 
     private void FlipSprite() {
@@ -44,13 +91,14 @@ public class PlayerMovement : MonoBehaviour {
 
     }
 
-    private void OnJump(InputValue value) {
-        bool isPlayerGrounded = playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+    private bool isPlayerMovingVertical() {
+        return Mathf.Abs(playerRigidbody.velocity.y) > Mathf.Epsilon;
+    }
 
-        if (!isPlayerGrounded) { return;}
-        else { 
-            playerRigidbody.velocity += new Vector2(0f, jumpSpeed);
-        }
+    private void OnJump(InputValue value) {
+        if (!canJump) { return;}
+
+        playerRigidbody.velocity += new Vector2(0f, jumpSpeed);
     }
 
     private void OnMove(InputValue value) {
