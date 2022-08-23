1bd2f592f2c45a6c182cc852f68357ef92273680
Eric Lee <szl0094@auburn.edu>
2022-08-23 16:40:40 -0500

    fixes an issue where animation doesn't resume after getting off a ladder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -57,6 +57,7 @@ public class PlayerMovement : MonoBehaviour {
             isPlayerClimbing = false;
             playerAnimator.SetBool("isClimbing", false);
             playerRigidbody.gravityScale = gravityScaleAtStart;
+            playerAnimator.StopPlayback();
             return;
         }
 
