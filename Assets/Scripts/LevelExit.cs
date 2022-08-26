e7c6c05182c777fe341024543b395ab0236ac0e0
Eric Lee <szl0094@auburn.edu>
2022-08-26 15:21:45 -0500

    Loading next Level implemented

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+    [SerializeField] private float _levelLoadDelay = 1;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+
+    private IEnumerator LoadNextLevel() {
+        yield return new WaitForSecondsRealtime(_levelLoadDelay);
+
+        int currentScenIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentScenIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
