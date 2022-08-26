8c6ebcfbf6a749a3610dacbd8715d5d0d26329d8
Eric Lee <szl0094@auburn.edu>
2022-08-26 16:29:53 -0500

    Coin Pick up sound added. First coin difficulty lowered.

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinPickup : MonoBehaviour
-{
+public class CoinPickup : MonoBehaviour {
+    [SerializeField] AudioClip coinPickupSFX;
+
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player" && collision.GetType() == typeof(CapsuleCollider2D)) {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position, 0.5f);
             Destroy(gameObject);
             FindObjectOfType<GameSession>().AddScore();
         }
